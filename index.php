6424a59127f631e1595bfbc7abbcb3e300ae5abe
khim <ito.benimun@gmail.com>
2026-06-10 15:36:44 +0545

    this is the way do f

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo '<h1>सुन्दर शुदुरपश्चिम प्रदेश !</h1>';
 echo '<h1>सुन्दर शुदुरपश्चिम प्रदेश !</h1>';
 echo '<h1>Gandaki!</h1>';
-echo'<img src="mnr.jpg"width="300">';
+echo'<img src="gandaki.jpg"width="300">';
 echo'<img src="mnr.jpg"gandaki="300">';
 ?>
